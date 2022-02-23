ae8da664510fce0ad0d9adc4980f054f4ce5a69b
Ad4mDev <96986853+Ad4mDev@users.noreply.github.com>
2022-02-23 19:33:44 +0100

    Create createchat.js

---
+++ commands/mod/createchat.js
@@ -0,0 +1,22 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+  config : {
+    name: "createchat",
+    description: "Create Voice Channels in your Server"},
+    run: async(client, message , args) => {
+    if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+    return message.channel.send("<:896365442925482025:914642294752301057> | **You don't have enough Permissions**")
+}
+    if (!args[0]) {
+    return message.channel.send("**Please mention the name for the Channel**")
+}
+    message.guild.channels.create(args.slice(0).join(" "), {type: "chat"});
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Channel Updates")
+    .setDescription(`Channel has been created`)
+    .setColor("2f3136");
+  message.channel.send(embed);
+}
+}
